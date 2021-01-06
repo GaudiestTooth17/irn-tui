bd9beb5d2cc00b4f7d7f7ec35cfc95835962ece6
GaudiestTooth17 <alex.r.anthon@gmail.com>
2021-01-06 13:10:51 -0700

    Finished most of the functions.

--- ui.py
+++ ui.py
@@ -4,12 +4,16 @@ This is a ui script for the infection resistant network project. The goal is to 
 all the tools together into one cohesive package to make experimenation easier.
 """
 import subprocess as sp
+import time
 from typing import Any, Optional, Dict
 
 
 GRAPH_DIR = 'graphs'
 BIN_DIR = 'bin'
 DISEASE_DIR = 'diseases'
+VIS_DIR = 'visualizations'
+SIM_BIN = f'{BIN_DIR}/infection-resistant-network'
+VIS_BIN = f'{BIN_DIR}/graph-visualizer'
 
 
 def main():
@@ -26,7 +30,7 @@ def main_menu():
     # a collection of choices with an index, plain English name, and a function to call
     choices = list(enumerate((('analyze graph', analyze_graph),
                              ('run simulation batch', run_simulation_batch),
-                             ('visualize simulation', visaulize_sim),
+                             ('visualize simulation', visualize_sim),
                              ('quit', exit))))
     valid_numbers = list(map(lambda c: c[0], choices))
 
@@ -47,16 +51,42 @@ def analyze_graph():
 
 
 def run_simulation_batch():
+    """
+    Runs a group of simulations and displays a summary of the results
+    """
     graph_name = select_file_from_dir(GRAPH_DIR, 'Select a graph to use:')
+    disease_name = select_file_from_dir(DISEASE_DIR, 'Select a disease to use:')
     num_simulations = int_input('How many simulations? ')
     sim_length = int_input('How many steps for each simulation? ')
-    output = run_cmd('bin/infection-resistant-network', graph_name,
+    output = run_cmd(SIM_BIN, disease_name, graph_name,
                      str(num_simulations), str(sim_length))
     print(output)
 
 
-def visaulize_sim():
-    print('visaulize simulation')
+def visualize_sim():
+    """
+    Runs a simulation and then opens the visualization program to show an animation
+    """
+    graph_name = select_file_from_dir(GRAPH_DIR, 'Select a graph to use:')
+    disease_name = select_file_from_dir(DISEASE_DIR, 'Select a disease to use:')
+    save_vis = bool_input('Would you like to save the visualization?')
+    if save_vis:
+        vis_name = input('Enter a name: ')
+        file_name = f'{VIS_DIR}/{vis_name}'
+    else:
+        file_name = f'/tmp/vis_{"".join(time.ctime())}'
+
+    output = run_cmd(SIM_BIN, disease_name, graph_name)
+    print(output.split('\n')[-1])
+    run_cmd(VIS_BIN, file_name)
+
+
+def load_visualization():
+    """
+    load a previously saved visualization and display it
+    """
+    visualization_name = select_file_from_dir(VIS_DIR, 'Select a visualization:')
+    run_cmd(VIS_BIN, visualization_name)
 
 
 def select_file_from_dir(dir_name: str, prompt: str) -> str:
@@ -89,6 +119,11 @@ def int_input(msg='') -> int:
     if x is None:
         x = int_input(msg)
     return x
+
+
+def bool_input(msg='') -> bool:
+    x = input(f'{msg} (y/n) ')
+    return x.lower() == 'y'
 
 
 def run_cmd(cmd, *args) -> str:
