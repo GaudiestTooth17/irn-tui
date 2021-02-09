40925c583444dcf227de70899021a5433c7b6084
GaudiestTooth17 <alex.r.anthon@gmail.com>
2021-02-09 15:54:39 -0700

    Added a gui. Started adding simulation code in Python.

--- ui.py
+++ ui.py
@@ -4,22 +4,16 @@ This is a ui script for the infection resistant network project. The goal is to 
 all the tools together into one cohesive package to make experimenation easier.
 """
 import time
-from typing import Dict, Any
-from utility import run_cmd, int_input, bool_input, float_input
-
-
-GRAPH_DIR = 'graphs'
-BIN_DIR = 'bin'
-DISEASE_DIR = 'diseases'
-VIS_DIR = 'visualizations'
-TMP_DIR = '/tmp/irn-ui-vis'
-PLOT_DIR = 'plots'
-SIM_BIN = f'{BIN_DIR}/infection-resistant-network'
-VIS_BIN = f'{BIN_DIR}/graph-visualizer'
+from typing import Dict, Any, Tuple
+from utility import run_cmd, int_input, bool_input, float_input, int_list_input, read_adj_list
+from inspect import getmembers, isfunction
+import analysis as an
+import constants
+import networkx as nx
 
 
 def main():
-    run_cmd('mkdir', TMP_DIR, silent=True)
+    run_cmd('mkdir', constants.TMP_DIR, silent=True)
     while True:
         next_function = main_menu()
         next_function()
@@ -51,20 +45,30 @@ def main_menu():
 
 def analyze_graph():
     # TODO: add the go program for analyzing graphs
-    graph_name = select_file_from_dir(GRAPH_DIR, 'Select a graph to analyze:')
+    graph_name = select_file_from_dir(constants.GRAPH_DIR, 'Select a graph to analyze:')
+    choices = dict(enumerate(getmembers(an, isfunction)))
 
-    print('you selected', graph_name)
+    selections = [-1]
+    while not all((s in choices.keys() for s in selections)):
+        print('Select the analyses to run:')
+        print_func_choices(choices)
+        selections = int_list_input('? ')
+
+    analyses = (choices[sel][1] for sel in selections)
+    graph = nx.Graph(read_adj_list(graph_name))
+    for analysis in analyses:
+        print(analysis(graph))
 
 
 def run_simulation_batch():
     """
     Runs a group of simulations and displays a summary of the results
     """
-    graph_name = select_file_from_dir(GRAPH_DIR, 'Select a graph to use:')
-    disease_name = select_file_from_dir(DISEASE_DIR, 'Select a disease to use:')
+    graph_name = select_file_from_dir(constants.GRAPH_DIR, 'Select a graph to use:')
+    disease_name = select_file_from_dir(constants.DISEASE_DIR, 'Select a disease to use:')
     num_simulations = int_input('How many simulations? ')
     sim_length = int_input('How many steps for each simulation? ')
-    output = run_cmd(SIM_BIN, disease_name, graph_name,
+    output = run_cmd(constants.SIM_BIN, disease_name, graph_name,
                      str(num_simulations), str(sim_length))
     print()
     print(output)
@@ -74,31 +78,31 @@ def visualize_sim():
     """
     Runs a simulation and then opens the visualization program to show an animation
     """
-    graph_name = select_file_from_dir(GRAPH_DIR, 'Select a graph to use:')
-    disease_name = select_file_from_dir(DISEASE_DIR, 'Select a disease to use:')
+    graph_name = select_file_from_dir(constants.GRAPH_DIR, 'Select a graph to use:')
+    disease_name = select_file_from_dir(constants.DISEASE_DIR, 'Select a disease to use:')
     save_vis = bool_input('Would you like to save the visualization?')
     if save_vis:
         vis_name = input('Enter a name: ')
-        file_name = f'{VIS_DIR}/{vis_name}.txt'
+        file_name = f'{constants.VIS_DIR}/{vis_name}.txt'
     else:
-        file_name = f'{TMP_DIR}vis_{"".join(time.ctime().split(" "))}.txt'
+        file_name = f'{constants.TMP_DIR}vis_{"".join(time.ctime().split(" "))}.txt'
 
-    output = run_cmd(SIM_BIN, disease_name, graph_name)
+    output = run_cmd(constants.SIM_BIN, disease_name, graph_name)
     print(output.split('\n')[-1])
     with open(file_name, 'w') as vis_file:
         with open(graph_name, 'r') as graph_file:
             graph_text = graph_file.readlines()
             vis_file.writelines(graph_text + ([] if graph_text[-1] == '\n' else ['\n']))
             vis_file.write(output)
-    run_cmd(VIS_BIN, file_name)
+    run_cmd(constants.VIS_BIN, file_name)
 
 
 def load_visualization():
     """
     load a previously saved visualization and display it
     """
-    visualization_name = select_file_from_dir(VIS_DIR, 'Select a visualization:')
-    run_cmd(VIS_BIN, visualization_name)
+    visualization_name = select_file_from_dir(constants.VIS_DIR, 'Select a visualization:')
+    run_cmd(constants.VIS_BIN, visualization_name)
 
 
 def create_new_disease():
@@ -110,7 +114,7 @@ def create_new_disease():
     time_to_r = int_input('How many steps to transition to removed state? ')
     inf_prob = float_input('What is the probability of transmission to neighbors? ')
     start_num_infected = int_input('How many nodes should be infectious at the start? ')
-    with open(f'{DISEASE_DIR}/{disease_name}.txt', 'w') as dis_file:
+    with open(f'{constants.DISEASE_DIR}/{disease_name}.txt', 'w') as dis_file:
         dis_file.write(f'{time_to_i} {time_to_r} {inf_prob} {start_num_infected}\n')
 
 
@@ -137,6 +141,15 @@ def print_choices(choices: Dict[int, Any]) -> None:
     """
     for choice in choices.items():
         print(choice[0], choice[1])
+
+
+def print_func_choices(choices: Dict[int, Tuple[str, Any]]) -> None:
+    """
+    A collection of function names with corresponding integers is displayed so that users
+    only have to type a number to choose one. The Any in the type signature is actually a function.
+    """
+    for choice in choices.items():
+        print(choice[0], choice[1][0])
 
 
 if __name__ == "__main__":
--- utility.py
+++ utility.py
@@ -1,5 +1,11 @@
-from typing import Any, Optional
+from typing import Any, Optional, List, Tuple
 import subprocess as sp
+import matplotlib.pyplot as plt
+import collections
+import numpy as np
+import networkx as nx
+from typing import Dict
+from sim import Disease, SEIRInitFunc
 
 
 def safe_cast(obj: Any, T) -> Optional[int]:
@@ -43,6 +49,14 @@ def bool_input(msg='') -> bool:
     return x.lower() == 'y'
 
 
+def int_list_input(msg='') -> List[int]:
+    xs = input(msg)
+    ints = [safe_cast(x, int) for x in xs.split(' ')]
+    if any((x is None for x in ints)):
+        int_list_input(msg)
+    return ints
+
+
 def run_cmd(cmd, *args, silent=False) -> str:
     """
     Runs a terminal command.
@@ -60,3 +74,127 @@ def run_cmd(cmd, *args, silent=False) -> str:
             print('\nCould not execute command\n')
             print(e)
         return ''
+
+
+class DirContents:
+    def __init__(self, dir_name: str, contents: List[str]) -> None:
+        self.dir_name = dir_name
+        self.contents = contents
+
+    def __getitem__(self, item):
+        if item in self.contents:
+            return f'{self.dir_name}/{item}'
+        raise Exception(f'{item} NOT FOUND')
+
+    def __len__(self):
+        return len(self.contents)
+
+
+def ls_dir(dir: str) -> DirContents:
+    files = [fn for fn in run_cmd('ls', dir).split('\n') if len(fn) > 1]
+    return DirContents(dir, files)
+
+
+def read_adj_list(file_name) -> np.ndarray:
+    """
+    This reads in the data from half a symmetric matrix and mirrors it.
+    If the whole matrix is present in the file, that won't cause problems.
+    This cannot read unsymmetric matrices.
+    """
+    with open(file_name, 'r') as f:
+        line = f.readline()
+        shape = (int(line[:-1]), int(line[:-1]))
+        matrix = np.zeros(shape, dtype=np.uint8)
+
+        line = f.readline()[:-1]
+        i = 1
+        while len(line) > 0:
+            coord = line.split(' ')
+            matrix[int(coord[0]), int(coord[1])] = 1
+            matrix[int(coord[1]), int(coord[0])] = 1
+            line = f.readline()[:-1]
+            i += 1
+    return matrix
+
+
+def show_deg_dist_from_matrix(matrix: np.ndarray, title, *, color='b', display=False, save=False):
+    """
+    This shows a degree distribution from a matrix.
+    :param matrix: The matrix.
+    :param title: The title.
+    :param color: The color of the degree distribution.
+    :param display: Whether or not to display it.
+    :param save: Whether or not to save it.
+    :return: None
+    """
+    graph = nx.from_numpy_matrix(matrix)
+    degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)
+    degree_count = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degree_count.items())
+
+    fig, ax = plt.subplots()
+    plt.bar(deg, cnt, width=0.80, color=color)
+
+    plt.title(title)
+    plt.ylabel("Count")
+    plt.xlabel("Degree")
+    ax.set_xticks([d + 0.4 for d in deg])
+    ax.set_xticklabels(deg)
+
+    if display:
+        plt.show()
+        # print(title + ' displayed')
+    if save:
+        plt.savefig(title)
+        with open(title + '.csv', 'w') as file:
+            for i in range(len(cnt)):
+                file.write(f'{deg[i]},{cnt[i]}\n')
+        # print(title + ' saved')
+    plt.clf()
+
+
+def make_node_to_degree(adj_mat) -> List[int]:
+    node_to_degree = [0 for _ in range(adj_mat.shape[0])]
+    for i in range(adj_mat.shape[0]):
+        for j in range(adj_mat.shape[1]):
+            if adj_mat[i][j] > 0:
+                node_to_degree[i] += 1
+    return node_to_degree
+
+
+def show_clustering_coefficent_dist(node_to_coefficient: Dict[int, float], node_to_degree: Dict[int, int]) -> None:
+    degree_to_avg_coefficient = {}
+    for node, coefficient in node_to_coefficient.items():
+        if node_to_degree[node] not in degree_to_avg_coefficient:
+            degree_to_avg_coefficient[node_to_degree[node]] = []
+        degree_to_avg_coefficient[node_to_degree[node]].append(coefficient)
+    for degree, coefficients in degree_to_avg_coefficient.items():
+        degree_to_avg_coefficient[degree] = sum(coefficients)/len(coefficients)
+
+    plot_data = list(degree_to_avg_coefficient.items())
+    plot_data.sort(key=lambda e: e[0])
+    plt.plot(tuple(e[0] for e in plot_data), tuple(e[1] for e in plot_data))
+    plt.xlabel('degree')
+    plt.ylabel('average clustering coefficient')
+
+    avg_clustering_coefficient = sum((e[1] for e in plot_data)) / len(plot_data)
+    print(f'Average clustering coefficient for all nodes: {avg_clustering_coefficient}')
+
+    plt.show()
+
+
+def read_disease(file_name: str) -> Tuple[Disease, SEIRInitFunc]:
+    with open(file_name, 'r') as f:
+        fields = f.readline().split(' ')
+    disease = Disease(int(fields[0]), int(fields[1]), float(fields[2]))
+
+    num_to_infect = int(fields[4])
+
+    def init_func(num_nodes: int) -> np.ndarray:
+        nonlocal num_to_infect
+        # the four is for the four states in SEIR
+        seir = np.zeros((num_nodes, 4), dtype=np.int32)
+        seir[np.random.randint(seir.shape[0], size=num_to_infect)] = 1
+        return seir
+
+    return disease, init_func
